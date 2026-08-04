bc1856816a2e71077c7291faf8c690296de772ef
Amar Choudhary <101505261+amar-jr@users.noreply.github.com>
2026-08-04 19:58:51 +0530

    Time: 6 ms (7.67%), Space: 63.1 MB (11.35%) - LeetHub

--- 0011-container-with-most-water/0011-container-with-most-water.cpp
+++ 0011-container-with-most-water/0011-container-with-most-water.cpp
@@ -4,11 +4,13 @@ public:
         
         int ans=INT_MIN;
         int s=height.size();
-
+        
         int i=0,e=s-1;
+
         while(i<e){
-            ans=max(ans, (min(height[i],height[e])*(e-i)));
-            if(height[i]<=height[e]) i++;
+
+            ans=max(ans, min(height[i],height[e])*(e-i));
+            if(height[i]<height[e]) i++;
             else e--;
         }
         return ans;
