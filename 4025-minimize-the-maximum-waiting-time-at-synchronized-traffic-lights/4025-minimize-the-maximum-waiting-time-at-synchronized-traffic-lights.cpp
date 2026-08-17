2f72449fe8efb655fe3d45fe6492aa394e0b8d5d
Amar Choudhary <101505261+amar-jr@users.noreply.github.com>
2026-08-17 20:02:12 +0530

    Time: 17 ms (42.86%), Space: 178.6 MB (100%) - LeetHub

---
+++ 4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights/4025-minimize-the-maximum-waiting-time-at-synchronized-traffic-lights.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minPenalty(int period, vector<int>& lights, vector<int>& arrivalTime) {
+        int ans=INT_MIN;
+        sort(lights.begin(),lights.end());
+        int n=lights.size()-1;
+        for(int i=0;i<arrivalTime.size();i++){
+            int waiting=0;
+            for(int j=n; j>=0;j--){
+                int extratime= arrivalTime[i]%period;
+                if(lights[j]>extratime){
+                    waiting=0;
+                    break;
+                }
+                else{
+                    waiting=max(waiting,period-extratime);
+                    j=j/2;
+                }
+            }
+            ans=max(ans,waiting);
+        }
+        if(ans==INT_MIN) ans=0;
+        return ans;
+    }
+};
